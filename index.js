b90c41e8744ebb3be0db0b0208fbd22e76f1e14d
AlamNurul95 <nurulalamsm95@gmail.com>
2024-04-01 18:00:23 +0100

    1st April

--- index.js
+++ index.js
@@ -73,6 +73,16 @@ app.post('/booking',async(req,res)=>{
     res.send(result);
 });
 
+//Delete
+app.delete('/booking/:id',async(req,res)=>{
+  const id=req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result=await ordersCollection.deleteOne(query);
+  res.send(result);
+});
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
